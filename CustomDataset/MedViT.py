c67966190a336ed8bbe0bd72843fa216800395fe
noaft <nvantoan1203@gmail.com>
2025-04-18 01:15:05 +0700

    123

--- CustomDataset/MedViT.py
+++ CustomDataset/MedViT.py
@@ -497,8 +497,8 @@ class MedViT(nn.Module):
                     nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        x = self.stem(x)
         x_add = self.cnn(x)
+        x = self.stem(x)
         for idx, layer in enumerate(self.features):
             if self.use_checkpoint:
                 x = checkpoint.checkpoint(layer, x)
@@ -516,6 +516,8 @@ class Extract_features(nn.Module):
         super(Extract_features, self).__init__()
     
         self.conv1 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -526,7 +528,16 @@ class Extract_features(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
 
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
         x = self.flatten(x)
+
         
         return x
 
